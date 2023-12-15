d068ddc64980d1a193b654d07b726c030a82ae4d
Семён Иванов <lilepopendr1@mail.ru>
2023-12-15 18:58:44 +0500

    Task1

---
+++ Tyuiu.KrigerAE.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KrigerAE.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c > d);//true
+            res[1] = (a < b) & (c < d);//false
+            res[2] = (a + 20 > b) || (c < d);//true
+            res[3] = (a > b) && (c > d);//true
+            res[4] = !res[1];//true
+            res[5] = (a + 20 < b) ^ (c < d + 50);//false
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KrigerAE.Sprint2.Task1.V17/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KrigerAE.Sprint2.Task1.V17.Lib;
+namespace Tyuiu.KrigerAE.Sprint2.Task1.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 135;
+            int b = 123;
+            int c = 455;
+            int d = 321;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #1 | Выполнил: Кригер А.Э. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Кригер Артур Эдуардович | ИИПБ-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("* ==, !=, <, >, <=, >=, последовательность операций не должна нарушаться  *");
+            Console.WriteLine("*  и арифметических выражений, которая вернет логическую                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+            
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
